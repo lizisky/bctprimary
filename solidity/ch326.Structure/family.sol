a9e24d1ba865936c68d6b10b4b7235212dcb4d21
lizitime <abcd@qq.com>
2020-07-04 16:15:02 +0800

    add structure

---
+++ solidity/ch326.Structure/family.sol
@@ -0,0 +1,27 @@
+/******************************************************************************
+file name   : family.sol
+author      : Kevin Zhang
+vendor      : 北京粒子时代科技有限公司(http://lizitime.com)
+location    : https://github.com/lizitime/bctprimary/
+description : 工业和信息化部-区块链应用工程师（初级）培训教程配套源码
+******************************************************************************/
+// SPDX-License-Identifier: GPLv3
+
+pragma solidity ^0.6.0;
+
+contract Family {
+
+    struct Person {
+        bytes32 name;
+        uint32 age;
+    }
+    Person   leader;
+
+    function setAge(uint32 ag) public {
+        leader.age = ag;
+    }
+    
+    function getAge() public view returns (uint32) {
+        return leader.age;
+    }
+}
