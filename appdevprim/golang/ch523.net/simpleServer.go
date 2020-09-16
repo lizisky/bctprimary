d9e8a79815b160ebb37251dd63d4b393759c2552
Kevin Zhang <kevin@kmbp.local>
2020-09-16 10:54:59 +0800

    revise

--- appdevprim/golang/ch523.net/simpleServer.go
+++ appdevprim/golang/ch523.net/simpleServer.go
@@ -1,9 +1,9 @@
 /******************************************************************************
-file name   : simpleServer.go
-author      : Kevin Zhang
-vendor      : 北京粒子时代科技有限公司(http://liziblockchain.com)
-location    : https://github.com/liziblockchain/bctprimary/
-description : 工业和信息化部-区块链应用工程师（初级）培训教程配套源码
+File name   : simpleServer.go
+Author      : Kevin Zhang
+Vendor      : 粒子区块链(http://liziblockchain.com)
+Location    : https://github.com/liziblockchain/bctprimary/
+Description : 工业和信息化部-区块链应用工程师（初级）培训教程配套源码
 ******************************************************************************/
 
 package main
--- basic_concept/solidity/ch323.introduction/web3js/helloWorld.js
+++ basic_concept/solidity/ch323.introduction/web3js/helloWorld.js
@@ -1,9 +1,9 @@
 /******************************************************************************
-file name   : helloWorld.js
-author      : Kevin Zhang
-vendor      : 北京粒子时代科技有限公司(http://liziblockchain.com)
-location    : https://github.com/liziblockchain/bctprimary/
-description : 工业和信息化部-区块链基础理论课程（初级）培训教程配套源码
+File name   : helloWorld.js
+Author      : Kevin Zhang
+Vendor      : 粒子区块链(http://liziblockchain.com)
+Location    : https://github.com/liziblockchain/bctprimary/
+Description : 工业和信息化部-区块链基础理论课程（初级）培训教程配套源码
 ******************************************************************************/
 
 "use strict";
