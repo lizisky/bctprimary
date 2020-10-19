f37e3819813ed26af9ad85f28521d35bbebcc237
Kevin Zhang <kevin@kmbp.local>
2020-10-19 16:14:32 +0800

    revise

---
+++ productMgrPrim/exercise/helloWorld/helloWorld.sol
@@ -0,0 +1,22 @@
+/******************************************************************************
+File name   : helloWorld.sol
+Author      : Kevin Zhang
+Vendor      : 粒子区块链(http://liziblockchain.com)
+Location    : https://github.com/liziblockchain/bctprimary/
+Description : 工业和信息化部-区块链行业产品经理（初级）培训教程配套源码
+******************************************************************************/
+// SPDX-License-Identifier: GPLv3
+
+pragma solidity >=0.6.0 <0.7.0;
+
+contract HelloWorld {
+    uint256 number;
+
+    function set(uint256 num) public {
+        number = num;
+    }
+
+    function get() public view returns (uint256){
+        return number;
+    }
+}
